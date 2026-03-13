80b8df4d4edd8f907d26269e2aad587a4f5bb2e6
Douaaa <douaabhl7@gmail.com>
2026-03-12 22:29:20 -0400

    essai2
    chang

--- src/backend/plantdata/urls.py
+++ src/backend/plantdata/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_plant_data, name='add_plant_data'),
+    path('stats/', views.get_stats, name='get_stats'),
+    path('latest/', views.get_latest, name='get_latest'),
 ]
